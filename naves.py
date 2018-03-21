9e53f3838d21340730145efd950c6075901863a4
Hector Montillo <hector.montillo@utp.edu.co>
2018-03-21 18:57:28 -0500

    Se cambio el movimiento de la nave(a s w d), se agrego un disparo con el click, se agrego un punto de mira

--- naves.py
+++ naves.py
@@ -5,21 +5,91 @@ import random
 ANCHO = 1000
 ALTO = 500
 SIZE = (ANCHO, ALTO)
+FPS = 60
 
 BLACK = (0,0,0)
 RED = (255,0,0)
 VERDE = (0,255,0)
 AZUL = (0,0,255)
+AMARILLO = (255,255,0)
 
 #CLASES
 class Nave(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((20,20))
+        self.image = pg.Surface((32,32))
         self.image.fill(AZUL)
         self.rect = self.image.get_rect()
         self.rect.left = 32
         self.rect.centery = ALTO/2
+        self.velx = 0
+        self.vely = 0
+        self.vel = 5
+
+
+    def update(self):
+        #self.rect.center = pg.mouse.get_pos()
+        self.velx = 0
+        self.vely = 0
+        keystate = pg.key.get_pressed()
+
+        if keystate[pg.K_SPACE]:
+            self.vel = 10
+        else:
+            self.vel = 5
+
+        if keystate[pg.K_LEFT] or keystate[pg.K_a]:
+            self.velx = -self.vel
+        if keystate[pg.K_RIGHT] or keystate[pg.K_d]:
+            self.velx = self.vel
+        self.rect.x += self.velx
+
+        if keystate[pg.K_UP] or keystate[pg.K_w]:
+            self.vely = -self.vel
+        if keystate[pg.K_DOWN] or keystate[pg.K_s]:
+            self.vely = self.vel
+        self.rect.y += self.vely
+
+        if self.rect.right > ANCHO:
+            self.rect.right = ANCHO
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > ALTO:
+            self.rect.bottom = ALTO
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+    def disparar(self):
+        bala = Bala(self.rect.center)
+        BALAS.add(bala)
+        TODOS.add(bala)
+
+class Mira(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((8,8))
+        self.image.fill(VERDE)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.center = pg.mouse.get_pos()
+
+
+
+class Bala(pg.sprite.Sprite):
+    def __init__(self,xy):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((8,8))
+        self.image.fill(AMARILLO)
+        self.rect = self.image.get_rect()
+        self.rect.center = xy
+        self.vel = 10
+
+    def update(self):
+        self.rect.x += self.vel
+
+        if self.rect.left > ANCHO:
+            self.kill()
 
 
 
@@ -29,6 +99,8 @@ if __name__=="__main__":
     pg.init()
     ventana = pg.display.set_mode(SIZE)
     pg.display.set_caption("Naves")
+    pg.mouse.set_visible(False)
+    reloj = pg.time.Clock()
 
     #CARGAR IMAGENES
 
@@ -36,11 +108,15 @@ if __name__=="__main__":
     #CREAR GRUPOS
     TODOS = pg.sprite.Group()
     PLAYERS = pg.sprite.Group()
+    BALAS = pg.sprite.Group()
 
     #CREAR OBJETOS
     nave = Nave()
     PLAYERS.add(nave)
     TODOS.add(nave)
+
+    mira = Mira()
+    TODOS.add(mira)
 
 
 
@@ -48,10 +124,15 @@ if __name__=="__main__":
     #CICLO PRINCIPAL
     stop = False
     while stop == False:
+        #RELOJ
+        reloj.tick(FPS)
+
         #EVENTOS
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 stop = True
+            if event.type == pg.MOUSEBUTTONDOWN:
+                nave.disparar()
 
         #ACTUALIZAR
         TODOS.update()
